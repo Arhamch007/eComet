5fff438a261c80fa137e2ae58109914cf3a3afa8
Abouzar Ijaz <abouzarijaz89@gmail.com>
2023-06-16 21:02:21 +0500

    first commit

---
+++ js/contact.js
@@ -0,0 +1,11 @@
+// function SendMail() {
+//     var params = {
+//      from_name : document.getElementById("fullname").value,
+//     email_id : document.getElementById("email_id").value,
+//     number_id : document.getElementById("numbere_id").value,
+//     message : document.getElementById("message").value
+//     }
+//     emailjs.send("service_l2dn5jo","template_s2d0j01",params).then(function(res){
+//         alert("success!"+ res.status);
+//     })
+// }
