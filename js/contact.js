3e7233e0966b1ff8676a3ad5932d3c856d5722e4
Abouzar Ijaz <abouzarijaz89@gmail.com>
2023-06-20 20:13:02 +0500

    contactcommit

--- js/contact.js
+++ js/contact.js
@@ -9,3 +9,28 @@
 //         alert("success!"+ res.status);
 //     })
 // }
+function sendMail() {
+  var params = {
+    name: document.getElementById("name").value,
+    email: document.getElementById("email").value,
+    message: document.getElementById("message").value,
+    number: document.getElementById("number").value,
+
+  };
+
+  const serviceID = "service_l2dn5jo";
+  const templateID = "template_s2d0j01";
+
+    emailjs.send(serviceID, templateID, params)
+    .then(res=>{
+        document.getElementById("name").value = "";
+        document.getElementById("email").value = "";
+        document.getElementById("number").value = "";
+        document.getElementById("message").value = "";
+        console.log(res);
+        alert("Your message sent successfully!!")
+
+    })
+    .catch(err=>console.log(err));
+
+}
