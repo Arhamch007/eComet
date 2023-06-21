c1a8f50d4f6275b438e0c11a1de496adc1780c8e
Abouzar Ijaz <abouzarijaz89@gmail.com>
2023-06-21 15:22:56 +0500

    fvdsfsdf

--- js/contact.js
+++ js/contact.js
@@ -9,28 +9,51 @@
 //         alert("success!"+ res.status);
 //     })
 // }
-function sendMail() {
-  var params = {
-    name: document.getElementById("name").value,
-    email: document.getElementById("email").value,
-    message: document.getElementById("message").value,
-    number: document.getElementById("number").value,
 
-  };
+// Working with emailjs
+// function sendMail() {
+//   var params = {
+//     name: document.getElementById("name").value,
+//     email: document.getElementById("email").value,
+//     number: document.getElementById("number").value,
+//     message: document.getElementById("message").value,
+//   };
 
-  const serviceID = "service_l2dn5jo";
-  const templateID = "template_s2d0j01";
+//   const serviceID = "service_l2dn5jo";
+//   const templateID = "template_s2d0j01";
 
-    emailjs.send(serviceID, templateID, params)
-    .then(res=>{
-        document.getElementById("name").value = "";
-        document.getElementById("email").value = "";
-        document.getElementById("number").value = "";
-        document.getElementById("message").value = "";
-        console.log(res);
-        alert("Your message sent successfully!!")
+//     emailjs.send(serviceID, templateID, params)
+//     .then(res=>{
+//         document.getElementById("name").value = "";
+//         document.getElementById("email").value = "";
+//         document.getElementById("number").value = "";
+//         document.getElementById("message").value = "";
+//         console.log(res);
+//         alert("Your message sent successfully!!")
 
-    })
-    .catch(err=>console.log(err));
+//     })
+//     .catch(err=>console.log(err));
 
-}
+// }
+
+
+
+
+var btn = document.getElementById('btn');
+btn.addEventListener('click', function(e){
+    e.preventDefault()
+    var name = document.getElementById('name').value;
+    var email = document.getElementById('email').value;
+    var number = document.getElementById('number').value;
+    var message = document.getElementById('message').value;
+    var body = 'name: '+name+ '<br> email: '+email+ '<br> number: '+number+ '<br> message: '+message;
+    Email.send({
+    SecureToken : "507d8b25-6a50-45b5-821b-97a1ec68caaf",
+    To : 'ecomet-technologies@gmail.com',
+    From : "ddamas850@gmail.com",
+    Subject : "Contact message",
+    Body : body
+}).then(
+message => alert(message)
+);
+})
