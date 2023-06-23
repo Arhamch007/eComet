5ddcec25baba0a794dcd8f969d459375013433d5
Abouzar Ijaz <abouzarijaz89@gmail.com>
2023-06-23 17:54:47 +0500

    hgfghfd

--- js/main.js
+++ js/main.js
@@ -10,6 +10,48 @@
  })
 
 
+ document.addEventListener("click",function (e){
+	if(e.target.classList.contains("gallery-item1")){
+		  const src = e.target.getAttribute("src");
+		  document.querySelector(".modal-img").src = src;
+		  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal1'));
+		  myModal.show();
+	}
+  })
+  document.addEventListener("click",function (e){
+	if(e.target.classList.contains("gallery-item2")){
+		  const src = e.target.getAttribute("src");
+		  document.querySelector(".modal-img").src = src;
+		  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal2'));
+		  myModal.show();
+	}
+  }) 
+  document.addEventListener("click",function (e){
+	if(e.target.classList.contains("gallery-item3")){
+		  const src = e.target.getAttribute("src");
+		  document.querySelector(".modal-img").src = src;
+		  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal3'));
+		  myModal.show();
+	}
+  }) 
+  document.addEventListener("click",function (e){
+	if(e.target.classList.contains("gallery-item4")){
+		  const src = e.target.getAttribute("src");
+		  document.querySelector(".modal-img").src = src;
+		  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal4'));
+		  myModal.show();
+	}
+  })
+   document.addEventListener("click",function (e){
+	if(e.target.classList.contains("gallery-item5")){
+		  const src = e.target.getAttribute("src");
+		  document.querySelector(".modal-img").src = src;
+		  const myModal = new bootstrap.Modal(document.getElementById('gallery-modal5'));
+		  myModal.show();
+	}
+  })
+
+
  document.querySelector('.navbar-toggle').addEventListener('click', function() {
 	this.classList.toggle('active');
   });
